01e398e1c60e8085bae5c93f8aefaf50a7f95571
Denise Fernandez <52894773+denise-fernandez@users.noreply.github.com>
2021-02-19 19:24:28 -0800

    bleh

--- Lab3/src/TestPlayer.java
+++ Lab3/src/TestPlayer.java
@@ -6,5 +6,20 @@ public class TestPlayer {
 
 
 
-    
+    //super help-desk class
+    public static class PlayerMaker
+    {
+        public enum PlayerModeType{
+            BEGINNER, INTERMEDIATE, ADVANCED
+        }
+
+        public static PlayerModeFactory makePlayerModeFactory(PlayerModeType type){
+            switch (type){
+                case BEGINNER:
+                    return new CharacterFactory();
+                case INTERMEDIATE:
+
+            }
+        }
+    }
 }
