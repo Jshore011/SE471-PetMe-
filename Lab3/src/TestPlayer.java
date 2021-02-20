45934302430e3300103a76cbc1b0ebba65560c98
Denise Fernandez <52894773+denise-fernandez@users.noreply.github.com>
2021-02-19 20:13:47 -0800

    made main and utility class in TestPlayer

--- Lab3/src/TestPlayer.java
+++ Lab3/src/TestPlayer.java
@@ -7,8 +7,7 @@ public class TestPlayer {
 
 
     //super help-desk class
-    public static class PlayerMaker
-    {
+    public static class PlayerMaker {
         public enum PlayerModeType{
             BEGINNER, INTERMEDIATE, ADVANCED
         }
@@ -16,10 +15,34 @@ public class TestPlayer {
         public static PlayerModeFactory makePlayerModeFactory(PlayerModeType type){
             switch (type){
                 case BEGINNER:
-                    return new CharacterFactory();
+                    return new BeginnerFactory();
                 case INTERMEDIATE:
-
+                    return new IntermediateFactory();
+                case ADVANCED:
+                    return new AdvancedFactory();
+                default:
+                    throw new IllegalArgumentException("Player Mode does not exist. Enter valid mode.")
             }
         }
+    }
+
+    public static void main(String[] args) {
+
+        TestPlayer app = new TestPlayer();
+
+        System.out.println("Beginner Mode");
+        app.createPlayer(PlayerMaker.makePlayerModeFactory(PlayerMaker.PlayerModeType.BEGINNER));
+        System.out.println(app.getWeapon().getDescription());
+        System.out.println(app.getCharacter().getDescription());
+
+        System.out.println("Intermediate Mode");
+        app.createPlayer(PlayerMaker.makePlayerModeFactory(PlayerMaker.PlayerModeType.INTERMEDIATE));
+        System.out.println(app.getWeapon().getDescription());
+        System.out.println(app.getCharacter().getDescription());
+
+        System.out.println("Advanced Mode");
+        app.createPlayer(PlayerMaker.makePlayerModeFactory(PlayerMaker.PlayerModeType.ADVANCED));
+        System.out.println(app.getWeapon().getDescription());
+        System.out.println(app.getCharacter().getDescription());
     }
 }
