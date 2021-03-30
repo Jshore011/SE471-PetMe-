451a39434d0b008d841bb8cb881b3199ea5191f3
jessica Shore <shore011@cougars.csusm.edu>
2021-03-29 19:31:12 -0700

    notificationcollection actin a fool

--- lab6/src/NotificationCollection.java
+++ lab6/src/NotificationCollection.java
@@ -4,29 +4,21 @@ import java.util.ArrayList;
 
 public class NotificationCollection implements NotificationCollectionIF {
     private Notification items;
-    private int length = 0;
-    private int index = 0;
-    ArrayList<String> notifications = new ArrayList<String>();
+    ArrayList<String> notifications;
 
     NotificationCollection()
     {
-        notifications.addItem("Notification 1");
-        notifications.addItem("Notification 2");
-        notifications.addItem("Notification 3");
+        notifications = new ArrayList<>();
     }
-    public Notification getItem(int index)
+    public int getItem(int index)
     {
-        return this.items;
+        return notifications.get(index);
+        
+
+    public int getLength()
+    {
+        return notifications.length();
     }
 
-    public void addItem(String str)
-    {
-        length = length + 1;
 
-    }
-
-    public int getLength ()
-    {
-        return this.length;
-    }
 }
