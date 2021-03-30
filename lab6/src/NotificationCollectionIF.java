c3fe552f332296603286dfc8207846a3d8d7e53f
Ria Sharma <ria.sharma24@yahoo.com>
2021-03-29 21:29:50 -0700

    collectionIF

--- lab6/src/NotificationCollectionIF.java
+++ lab6/src/NotificationCollectionIF.java
@@ -1,3 +1,7 @@
+import java.util.Iterator;
+
 public interface NotificationCollectionIF {
     NotificationIteratorIF createIterator();
+    Iterator iterator = notification.createIterator();
+    while (iterator.hasNext()) {Notification notification = iterator.next();}
 }
