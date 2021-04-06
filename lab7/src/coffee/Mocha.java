c558c74a96ed1fa6e6928214141cd69a79c24644
jessica Shore <shore011@cougars.csusm.edu>
2021-04-06 15:56:39 -0700

    mocha class

--- lab7/src/coffee/Mocha.java
+++ lab7/src/coffee/Mocha.java
@@ -1,7 +1,9 @@
 package coffee;
 
-public class Mocha {
+import condiments.CoffeeOS_API;
 
+public class Mocha {
+    private CoffeeOS_API program;
     //returns the name of the program
     public String getName()
     {
@@ -12,6 +14,7 @@ public class Mocha {
     public void start()
     {
         //1. Indicate the machine as running and display the purchased type of coffee to LED.
+        program.setPowerLED(1);
        // 2. Grind coffee beans for 5 seconds.
       //  3. Heat up the water to 200 degree F and hold the temperature for 5 seconds.
       //  4. Add cream and vanilla once.
