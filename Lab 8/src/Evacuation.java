0f79a869e524d80f5863f49ba9ae082994bc691f
jessica Shore <shore011@cougars.csusm.edu>
2021-04-13 19:52:14 -0700

    hazard class

--- Lab 8/src/Evacuation.java
+++ Lab 8/src/Evacuation.java
@@ -6,7 +6,7 @@ public class Evacuation extends Decision{
         //demands all employees to evacuate
 
         //evacuation is initiated by ceo
-        
+
         //evac starts with all workers first, then supervisors, managers, CEO
 
         //when a person's evacuate() method is called it displays
--- Lab 8/src/Hazard.java
+++ Lab 8/src/Hazard.java
@@ -1,8 +1,14 @@
 
-public enum Hazard {
-	GasLeak,
-	Electrcity,
-	Water,
-	Natural
+public class Hazard {
+	private String type;
+	private String desp;
+	private int hazard_level;
+
+	public Hazard(String type, String desp, int hazard_level)
+	{
+		this.type=type;
+		this.desp=desp;
+		this.hazard_level=hazard_level;
+	}
 	
 }
