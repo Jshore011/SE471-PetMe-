9156f3072d4dbec9c182a8e4ce3cdbde422236cd
Ria Sharma <ria.sharma24@yahoo.com>
2021-05-09 19:50:22 -0700

    the one with report manager

---
+++ Petmanage/src/PetManager/PetLogType.java
@@ -0,0 +1,46 @@
+package PetManager;
+
+/**
+ * Enum type to represent types of pete logs
+ */
+public enum PetLogType {
+    Diet,
+    Exercise,
+    Medicine;
+
+    /**
+     * Converts a string representation of the pet log type to it's interal type
+     * @param str String representation
+     * @return pet log type
+     */
+    public static PetLogType fromString(String str) {
+        if (str.equals("medicine")) {
+            return PetLogType.Medicine;
+        } else if (str.equals("exercise")) {
+            return PetLogType.Exercise;
+        } else {
+            return PetLogType.Diet;
+        }
+    }
+
+    /**
+     * Converts a PetLogType to it's string representation
+     * @return string representation
+     */
+    public String toString() {
+        String str = null;
+        switch(this) {
+            case Diet:
+                str = "diet";
+                break;
+            case Medicine:
+                str = "medicine";
+                break;
+            case Exercise:
+                str = "exercise";
+                break;
+        }
+
+        return str;
+    }
+}
---
+++ stephsPetMe+/src/LocationManager/PlacesAPI.java
@@ -0,0 +1,4 @@
+package LocationManager;
+
+public class PlacesAPI {
+}
