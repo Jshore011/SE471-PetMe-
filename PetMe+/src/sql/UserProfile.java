161bf382519fd704437ad4ac05adf0f55e177df1
Denise Fernandez <52894773+denise-fernandez@users.noreply.github.com>
2021-05-18 21:07:33 -0700

    fixed builder

--- PetMe+/src/sql/UserProfile.java
+++ PetMe+/src/sql/UserProfile.java
@@ -19,38 +19,4 @@ public class UserProfile {
         return name + " | " + email + " | " + lightmode;
     }
 
-    String getName() {
-        return name;
-    }
-    void setName(String name){
-        this.name=name;
-    }
-
-    String getIcon() {
-        return icon;
-    }
-    void seticon(String icon){
-        this.icon=icon;
-    }
-    String getPhone() {
-        return phone;
-    }
-    void setPhone(String phone){
-        this.phone=phone;
-    }
-
-    String getEmail() {
-        return email;
-    }
-    void setEmail(String email){
-        this.email=email;
-    }
-    
-    boolean getlightmode(){
-        return lightmode;
-    }
-    
-    void setlightmode(boolean lightmode){
-        this.lightmode=lightmode;
-    }
 }
